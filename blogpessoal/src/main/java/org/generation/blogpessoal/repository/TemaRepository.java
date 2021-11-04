ab0b1cc482f7fa306de79bdfc753f5cc4bcad65f
mayaralm <mmayaraleitedemelo@gmail.com>
2021-11-04 11:45:13 -0300

    Projeto blog pessoal

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package org.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.generation.blogpessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	
+	public List<Tema> findAllByDescricaoConteiningIgoreCase(String descricao);
+	
+
+}
+
+
